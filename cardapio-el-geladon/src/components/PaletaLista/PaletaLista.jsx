c338b85b4de747c24e615fa6e37b46883a4da3f1
JVictor1604 <josevictor8569@gmail.com>
2023-01-20 20:27:10 -0300

    commit 3.0

--- cardapio-el-geladon/src/components/PaletaLista/PaletaLista.jsx
+++ cardapio-el-geladon/src/components/PaletaLista/PaletaLista.jsx
@@ -1,6 +1,8 @@
+
+import PaletaListaitem from "components/PaletaListaitem/PaletaListaitem";
 import React, { useState } from "react";
-import "./PaletaLista.css";
 import { paletas } from "mocks/paletas.js";
+import "./PaletaLista.css";
 
 
 
@@ -19,43 +21,17 @@ function PaletaLista() {
         setPaletaSelecionada({ ...paletaSelecionada, ...paleta });
     }
 
-    const badgeCounter = (canRender, index) =>
-        Boolean(canRender) && (<span className="PaletaListaItem__badge"> {paletaSelecionada[index] || 0} </span>);
-
-    const removeButton = (canRender, index) =>
-        Boolean(canRender) && (<button className="Acoes__remover" onClick={() => removerItem(index)}>remover</button>);
-
-
-
     return (
         <div className="PaletaLista">
+
             {paletas.map((paleta, index) => (
-                <div className="PaletaListaItem" key={`PaletaListaItem-${index}`}>
-                    {badgeCounter(paletaSelecionada[index], index)}
-                    <div>
-                        <div className="PaletaListaItem__titulo">
-                            {paleta.titulo}
-                        </div>
-                        <div className="PaletaListaItem__preco">{`R$ ${paleta.preco.toFixed(2)}`};</div>
-                        <div className="PaletaListaItem__descricao">
-
-                            {paleta.descricao}
-                        </div>
-                        <div className="PaletaListaItem__acoes Acoes">
-
-                            <button className={`Acoes__adicionar ${!paletaSelecionada[index] && "Acoes__adicionar--preencher"}`} onClick={() => adicionarItem(index)}
-                            >adicionar</button>
-
-                            {removeButton(paletaSelecionada[index], index)}
-
-                        </div>
-                    </div>
-                    <img
-                        className="PaletaListaItem__foto"
-                        src={paleta.foto}
-                        alt={`Paleta de ${paleta.sabor}`}
-                    />
-                </div>
+               <PaletaListaitem 
+               paleta = {paleta}
+               quantidadeSelecionada = {paletaSelecionada[index]}
+               index = {index}
+               onAdd = {index => adicionarItem(index)}
+               onRemove = {index => removerItem(index)}
+               key={`PaletaListaItem-${index}`}/>
             ))}
         </div>
     );
--- cardapio-el-geladon/src/index.jsx
+++ cardapio-el-geladon/src/index.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import Home from "./views/Home/Home";
 import './assets/styles/main.css';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-
-  <Home />
-
-  
-   </React.StrictMode>,
-  document.getElementById("root")
+    <Home />
+  </React.StrictMode>
 );
